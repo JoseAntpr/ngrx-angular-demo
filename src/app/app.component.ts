659b7317a334063ea177025e5fdf212a7179e1b7
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-07-16 13:06:10 +0200

    ADD: add actions and reducer for father component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Store, Action } from '@ngrx/store';
+import { IncrementarAction, DecrementarAction } from './contador/contador.actions';
+
+interface AppState {
+  contador: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,15 +14,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   contador: number;
 
-  constructor() {
-    this.contador = 10;
+  constructor(private store: Store<AppState>) {
+    // this.contador = 10;
+    this.store.subscribe( state => {
+      this.contador = state.contador;
+    });
   }
 
   incrementar() {
-    this.contador += 1;
+    const action = new IncrementarAction();
+
+    this.store.dispatch(action);
   }
 
   decrementar() {
-    this.contador -= 1;
+    const action = new DecrementarAction();
+
+    this.store.dispatch(action);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
+import { contadorReducer } from './contador/contador.reducer';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,8 @@ import { NietoComponent } from './contador/nieto/nieto.component';
     NietoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot( { contador: contadorReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
